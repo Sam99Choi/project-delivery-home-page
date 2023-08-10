c339921cdde5eab22136869d8a416583fd835089
jeeonglee1 <jeeonglee1@gmail.com>
2023-08-10 15:48:24 -0300

    feat: change frete gratis images and input routes

--- src/components/ListaPratos/Salada.tsx
+++ src/components/ListaPratos/Salada.tsx
@@ -3,17 +3,17 @@ import Link from "next/link";
 
 export default function Salada() {
   return (
-    <Link href={""}>
+    <Link href={"/montana"}>
       <div className="flex flex-col hover:-translate-y-0.5 transition">
         <Image
           className="rounded-xl w-[220px] h-[147px] shadow-cinza-200 shadow-md"
-          src="/../public/images/salada.avif"
+          src="/../public/images-restaurant/montana/Salada.png"
           alt="salada"
-          width={215}
+          width={219}
           height={100}
-          />
+        />
         <p className="flex justify-center items-center pt-2 font-medium">
-          Salada Especial
+          Salada Montana
         </p>
       </div>
     </Link>
